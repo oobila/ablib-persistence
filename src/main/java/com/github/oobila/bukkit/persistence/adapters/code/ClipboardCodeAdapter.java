9e22c5b231a947d2a165a6c9e1bd29cb181f1580
BuildTools <unconfigured@null.spigotmc.org>
2024-09-25 16:47:32 +0100

    using UTF-8

--- src/main/java/com/github/oobila/bukkit/persistence/adapters/code/ClipboardCodeAdapter.java
+++ src/main/java/com/github/oobila/bukkit/persistence/adapters/code/ClipboardCodeAdapter.java
@@ -9,8 +9,8 @@ import com.sk89q.worldedit.extent.clipboard.io.ClipboardWriter;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 
 import static com.github.oobila.bukkit.common.ABCommon.log;
@@ -26,7 +26,7 @@ public class ClipboardCodeAdapter implements CodeAdapter<Clipboard> {
     @Override
     public Clipboard toObject(StoredData storedData) {
         try (
-                ByteArrayInputStream inputStream = new ByteArrayInputStream(storedData.getData().getBytes(StandardCharsets.UTF_8));
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(storedData.getData().getBytes());
                 ClipboardReader clipboardReader = BuiltInClipboardFormat.SPONGE_V3_SCHEMATIC.getReader(inputStream)
         ) {
             return clipboardReader.read();
@@ -39,11 +39,14 @@ public class ClipboardCodeAdapter implements CodeAdapter<Clipboard> {
 
     @Override
     public String fromObject(Clipboard clipboard) {
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(8192)) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(8192); FileOutputStream fos = new FileOutputStream("D:\\Minecraft\\Server\\plugins\\blaa.schem")) {
             try (ClipboardWriter clipboardWriter = BuiltInClipboardFormat.SPONGE_V3_SCHEMATIC.getWriter(outputStream)) {
                 clipboardWriter.write(clipboard);
             }
-            return outputStream.toString(StandardCharsets.UTF_8);
+            try (ClipboardWriter clipboardWriter = BuiltInClipboardFormat.SPONGE_V3_SCHEMATIC.getWriter(fos)) {
+                clipboardWriter.write(clipboard);
+            }
+            return outputStream.toString();
         } catch (IOException e) {
             log(Level.SEVERE, "Could not save clipboard.");
             log(Level.SEVERE, e);
