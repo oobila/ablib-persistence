ff50fffca9746a8683bee74b0e22fc5bf2c77425
BuildTools <unconfigured@null.spigotmc.org>
2025-04-14 15:00:40 +0100

    fixing bug

--- src/main/java/com/github/oobila/bukkit/persistence/caches/real/SimpleFileCache.java
+++ src/main/java/com/github/oobila/bukkit/persistence/caches/real/SimpleFileCache.java
@@ -4,10 +4,9 @@ import com.github.oobila.bukkit.persistence.adapters.code.MapOfConfigurationSeri
 import com.github.oobila.bukkit.persistence.adapters.storage.FileStorageAdapter;
 import com.github.oobila.bukkit.persistence.adapters.vehicle.DynamicVehicle;
 import com.github.oobila.bukkit.persistence.caches.standard.ReadAndWriteCache;
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
 @SuppressWarnings("unused")
-public class SimpleFileCache<K, V extends ConfigurationSerializable> extends ReadAndWriteCache<K, V> {
+public class SimpleFileCache<K, V> extends ReadAndWriteCache<K, V> {
 
     public SimpleFileCache(String pathString, Class<K> keyType, Class<V> valueType) {
         super(
