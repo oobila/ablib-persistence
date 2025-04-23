8126998a10deeab8756af17f7fa5f99f93f8b435
BuildTools <unconfigured@null.spigotmc.org>
2025-04-23 23:29:55 +0100

    adding save to transaction

--- src/main/java/com/github/oobila/bukkit/persistence/caches/WriteCache.java
+++ src/main/java/com/github/oobila/bukkit/persistence/caches/WriteCache.java
@@ -1,6 +1,7 @@
 package com.github.oobila.bukkit.persistence.caches;
 
 import com.github.oobila.bukkit.persistence.model.CacheItem;
+import org.bukkit.OfflinePlayer;
 
 import java.util.UUID;
 
@@ -9,5 +10,21 @@ public interface WriteCache<K, V, C extends CacheItem<K, V>> extends ReadCache<K
     void save();
 
     void save(UUID partition);
+
+    default void transaction(UUID partition, Runnable runnable) {
+        boolean loaded = isLoaded(partition);
+        if (!loaded) {
+            load(partition);
+        }
+        runnable.run();
+        if (!loaded) {
+            save(partition);
+            unload(partition);
+        }
+    }
+
+    default void transaction(OfflinePlayer player, Runnable runnable) {
+        transaction(player.getUniqueId(), runnable);
+    }
 
 }
