0b7459d1990511053ffa00e580dbcc5e4bbdda8f
BuildTools <unconfigured@null.spigotmc.org>
2025-04-23 23:49:59 +0100

    fixing bug

--- src/main/java/com/github/oobila/bukkit/persistence/model/OnDemandCacheItem.java
+++ src/main/java/com/github/oobila/bukkit/persistence/model/OnDemandCacheItem.java
@@ -3,6 +3,7 @@ package com.github.oobila.bukkit.persistence.model;
 import com.github.oobila.bukkit.persistence.adapters.storage.StoredData;
 import com.github.oobila.bukkit.persistence.caches.WriteCache;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
@@ -14,13 +15,13 @@ public class OnDemandCacheItem<K, V> extends CacheItem<K, V> {
     private final WriteCache<K, V, OnDemandCacheItem<K, V>> cache;
     private final UUID partition;
 
-    public OnDemandCacheItem(Class<V> type, UUID partition, K key, V data, StoredData storedData, WriteCache<K, V, OnDemandCacheItem<K, V>> cache) {
+    public OnDemandCacheItem(Class<V> type, UUID partition, K key, V data, StoredData storedData, @NonNull WriteCache<K, V, OnDemandCacheItem<K, V>> cache) {
         super(type, key, data, storedData);
         this.partition = partition;
         this.cache = cache;
     }
 
-    public OnDemandCacheItem(Class<V> type, UUID partition, K key, V data, long size, ZonedDateTime updatedDate, WriteCache<K, V, OnDemandCacheItem<K, V>> cache) {
+    public OnDemandCacheItem(Class<V> type, UUID partition, K key, V data, long size, ZonedDateTime updatedDate, @NonNull WriteCache<K, V, OnDemandCacheItem<K, V>> cache) {
         super(type, key, data, size, updatedDate);
         this.partition = partition;
         this.cache = cache;
